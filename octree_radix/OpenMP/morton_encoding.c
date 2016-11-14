a4bae80f5059ba932b565f8dbe131f9c46fcd326
achigeor <achigeor92@gmail.com>
2016-11-14 21:42:05 +0200

    OpenMP paralle morton_encoding

--- octree_radix/OpenMP/morton_encoding.c
+++ octree_radix/OpenMP/morton_encoding.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "math.h"
+#include "omp.h"
 
 #define DIM 3
 
@@ -21,13 +22,18 @@ inline unsigned long int mortonEncode_magicbits(unsigned int x, unsigned int y, 
 }
 
 /* The function that transform the morton codes into hash codes */ 
-void morton_encoding(unsigned long int *mcodes, unsigned int *codes, int N, int max_level){
-  
-  for(int i=0; i<N; i++){
-    // Compute the morton codes from the hash codes using the magicbits mathod
-    mcodes[i] = mortonEncode_magicbits(codes[i*DIM], codes[i*DIM + 1], codes[i*DIM + 2]);
-  }
-  
+void morton_encoding(unsigned long int *mcodes, unsigned int *codes, int N, int max_level) {
+
+    int i = 0;
+    #pragma omp parallel shared( mcodes, codes) private(i)
+    {
+
+        #pragma omp for schedule(guided) private(i)
+        for ( i=0; i < N; i++) {
+            // Compute the morton codes from the hash codes using the magicbits mathod
+            mcodes[i] = mortonEncode_magicbits(codes[i * DIM], codes[i * DIM + 1], codes[i * DIM + 2]);
+        }
+    }
 }
 
 
